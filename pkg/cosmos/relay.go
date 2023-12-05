9b095d55188f7ad9ede554bbff311b8fe3d0ab23
Fergal Gribben <fergal.gribben@smartcontract.com>
2023-12-05 00:33:33 +0000

    Rename OCR2KeepersProvider to AutomationProvider

--- pkg/cosmos/relay.go
+++ pkg/cosmos/relay.go
@@ -107,6 +107,6 @@ func (r *Relayer) NewMedianProvider(rargs types.RelayArgs, pargs types.PluginArg
 	return configProvider, err
 }
 
-func (r *Relayer) NewOCR2KeepersProvider(rargs types.RelayArgs, pargs types.PluginArgs) (types.OCR2KeepersProvider, error) {
-	return nil, errors.New("ocr2keepers is not supported for cosmos")
+func (r *Relayer) NewAutomationProvider(rargs types.RelayArgs, pargs types.PluginArgs) (types.AutomationProvider, error) {
+	return nil, errors.New("automation is not supported for cosmos")
 }
